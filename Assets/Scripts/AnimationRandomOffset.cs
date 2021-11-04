b261c8ac7d90224250c02fdd467a392016e5f46d
ZhihYuan <onomatope1221@gmail.com>
2021-11-04 15:16:32 +0800

    feat: import scripts for animation events

---
+++ Assets/Scripts/AnimationRandomOffset.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+public class AnimationRandomOffset : MonoBehaviour
+{
+    void Start() => GetComponent<Animator>().SetFloat("Offset", Random.Range(0.0f, 1.0f));
+}
---
+++ Assets/Scripts/HideWeapon.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+public class HideWeapon : MonoBehaviour
+{
+    [SerializeField] private GameObject Weapon;
+    public void Hide() => Weapon.SetActive(false);
+    public void Unhide() => Weapon.SetActive(true);
+}
