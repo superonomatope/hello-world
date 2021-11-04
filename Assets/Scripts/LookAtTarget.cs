3f0f32cb7a3b34d8075e637c6caa107f333c52fc
ZhihYuan <onomatope1221@gmail.com>
2021-11-04 15:17:34 +0800

    feat: add camera look at target in editor

---
+++ Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+[ExecuteInEditMode]
+public class LookAtTarget : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+    [SerializeField] private Camera _eventCamera;
+    private void LateUpdate()
+    {
+        if (_target && _eventCamera)
+            _eventCamera.transform.LookAt(_target.transform);
+    }
+}
